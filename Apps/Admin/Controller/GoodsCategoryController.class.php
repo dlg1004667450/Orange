aaa37e1ea08f88cb0706371fcda2395254094d55
NENER <714571611@qq.com>
2014-10-13 14:17:07 +0800

    0.0.1

--- Apps/Admin/Controller/GoodsCategoryController.class.php
+++ Apps/Admin/Controller/GoodsCategoryController.class.php
@@ -1,20 +1,39 @@
 <?php
-namespace Admin\Controller;
-use Think\Controller;
-class GoodsCategoryController extends Controller {
-	
-	public  function  index(){
-		$model= M('goods_category'); 
-		$count      = $model->where('status=0')->count();
-		$Page       = new \Think\Page($count,2);
-		$show       = $Page->show();
-		$list = $model->where('status=0')->limit($Page->firstRow.','.$Page->listRows)->select();
-		$this->assign('list',$list);
-		$this->assign('page',$show);
-		$this->display('index/index'); 
-	}
-	public  function  addGoods(){
-	
-	}
 
+namespace Admin\Controller;
+
+use Think\Controller;
+
+/**
+ * 商品分类管理
+ *
+ * @author NENER
+ *        
+ */
+class GoodsCategoryController extends Controller {
+	public function index() {
+		$model = M ( 'goods_category' );
+		// 查询条件
+		$wherrArr = array (
+				'Status' => array (
+						'gt',
+						- 1 
+				) 
+		);
+		$allCount = $model->where ( $wherrArr )->count ();
+		$Page = new \Think\Page ( $allCount, 10 );
+		$showPage = $Page->show ();
+		$list = $model->where ( $wherrArr )->limit ( $Page->firstRow . ',' . $Page->listRows )->select ();
+		$this->assign ( 'list', $list );
+		$this->assign ( 'page', $showPage );
+		$this->display ( 'index/category' );
+	}
+	public function del() {
+		$id = I ( 'get.id' );
+		if ($id) {
+			$this->success("页面不存在",U('index'));
+		} else {
+			$this->success("页面不存在",U('index'));
+		}
+	}
 }
