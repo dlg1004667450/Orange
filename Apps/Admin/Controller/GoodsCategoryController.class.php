1d488c8815215b853a96badb5d97d58d36eb5498
Fingertipcc <Fingertipcc@163.com>
2014-10-14 09:23:55 +0800

    Revert "V 0.0.0.2"
    This reverts commit 74fe55e4cdccf3898d327261f5a5a90f65da8f8d.

--- Apps/Admin/Controller/GoodsCategoryController.class.php
+++ Apps/Admin/Controller/GoodsCategoryController.class.php
@@ -3,8 +3,6 @@
 namespace Admin\Controller;
 
 use Think\Controller;
-use Think\Model;
-use Org\Util\String;
 
 /**
  * 后台商品分类管理
@@ -25,12 +23,12 @@ class GoodsCategoryController extends Controller {
 						- 1 
 				) 
 		);
-		// 总数
+		//总数
 		$allCount = $model->where ( $wherrArr )->count ();
-		// 分页
+		//分页
 		$Page = new \Think\Page ( $allCount, 10 );
 		$showPage = $Page->show ();
-		// 分页查询
+		//分页查询
 		$list = $model->where ( $wherrArr )->limit ( $Page->firstRow . ',' . $Page->listRows )->select ();
 		$this->assign ( 'list', $list );
 		$this->assign ( 'page', $showPage );
@@ -40,70 +38,43 @@ class GoodsCategoryController extends Controller {
 	 * 删除
 	 */
 	public function del() {
-		$id = ( int ) I ( 'get.Id' );
+		$id = I ( 'get.Id' );
 		if ($id) {
 			$whereArr = array (
 					'Id' => $id 
 			);
-			$dal = M ();
-			$dal->startTrans (); // 开始事务
-			$model = M ( 'goods_category' );
-			$model->Status = - 1;
-			$r1 = $model->where ( $whereArr )->save (); // 操作1
-			$whereArrKw = array (
-					'CategoryId' => $id 
-			);
-			// 操作2
-			$r2 = $dal->execute ( "update goods_category_keyword set Status=-1 where CategoryId=" . $id );
-			if ($r1 && $r2) { // 成功
-				$dal->commit (); // 提交事务
+			if (M ( 'goods_category' )->where ( $whereArr )->delete ()) {
 				$this->success ( "操作成功", U ( 'index' ) );
 			} else {
-				$dal->rollback (); // 否则回滚
-				$this->error ( "操作失败", U ( 'index' ) );
+				$this->error ( "删除失败", U ( 'index' ) );
 			}
 		} else {
 			$this->error ( "页面不存在", U ( 'index' ) );
 		}
 	}
 	/**
-	 * 查询要修改的数据
+	 * 修改
 	 */
-	public function update() {
-		$id = ( int ) I ( 'get.Id' );
+	public function modif() {
+		$id = I ( 'get.Id' );
 		if ($id) {
 			$whereArr = array (
 					'Id' => $id 
 			);
-			$model = M ( 'goods_category' )->where ( $whereArr )->find ();
+			$model=M ( 'goods_category' )->where ( $whereArr )->find ();
 			if ($model) {
-				$whereArrKeyword = array (
-						'CategoryId' => $id 
+				$whereArrKeyword=array(
+					'CaregoryId'=>$id
 				);
-				$this->assign ( 'model', $model );
-				$this->assign ( 'modif', 'update' )->display ( 'index/modifcategory' );
+				$list=M('goods_category_keyword')->where($whereArrKeyword)->select();
+				
+				$this->assign ( 'list', $list );
+				$this->display ( 'index/category' );
 			} else {
-				$this->error ( "操作失败", U ( 'index' ) );
+				$this->error ( "删除失败", U ( 'index' ) );
 			}
 		} else {
-			$this->error ( "操作失败", U ( 'index' ) );
+			$this->error ( "删除失败", U ( 'index' ) );
 		}
-	}
-	/**
-	 * 保存
-	 * 包含更新 ，添加
-	 */
-	public function save() {
-		if (! IS_POST) {
-			$this->error ( "页面不存在" );
-		}
-		$model = M ( 'goods_category' );
-		$model->Title = I('Title');
-		$model->Presentation = I('Presentation');
-		$whereArr = array (
-				'Id' => ( int ) I ( "post.Id" ) 
-		);
-		$res= $model->where ( $whereArr )->save (); 
-		$this->success('操作成功',U('index'));
 	}
 }
--- Apps/Home/Controller/IndexController.class.php
+++ Apps/Home/Controller/IndexController.class.php
@@ -7,5 +7,8 @@ use Think\Controller;
 class IndexController extends Controller {
 	public function index() {
 		
+		redirect( U('good/index'));
+		// $news=M('News')->select();
+		$this->assign ( 'model', M ( 'News' )->select () )->display ( 'index' );
 	}
 }
