51d9766bc62379ca92080518fc3c0118d8a7770b
dlg1004667450 <1004667450@qq.com>
2014-10-14 17:07:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ if (version_compare ( PHP_VERSION, '5.3.0', '<' ))
 	die ( 'require PHP > 5.3.0 !' );
 define ( 'APP_DEBUG', True );
 define ( 'APP_NAME', 'Orange' );
-define ( 'APP_PATH', './Apps/' );
+define ( 'APP_PATH', './Apps/' );//123
 require './ORG/ThinkPHP/ThinkPHP.php';
