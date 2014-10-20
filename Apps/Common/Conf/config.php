5e40c25d92b03bee7cfdcce8811365127766da11
Fingertipcc <Fingertipcc@163.com>
2014-10-20 09:30:20 +0800

    ff

--- Apps/Common/Conf/config.php
+++ Apps/Common/Conf/config.php
@@ -1,14 +1,14 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
-		'DB_TYPE'               =>  'mysql',     // 数据库类型
-		'DB_HOST'               =>  '10.200.10.90', // 服务器地址
-		'DB_NAME'               =>  'juzidb',          // 数据库名
-		'DB_USER'               =>  'rootZ',      // 用户名
-		'DB_PWD'                =>  '8520',          // 密码
-				//'DB_USER'               =>  'zhoumeng',      // 用户名
-		//'DB_PWD'                =>  '8520',          // 密码
-		/* 模块相关配置 */
+	//'閰嶇疆椤�'=>'閰嶇疆鍊�'
+		'DB_TYPE'               =>  'mysql',     // 鏁版嵁搴撶被鍨�
+		'DB_HOST'               =>  'localhost', // 鏈嶅姟鍣ㄥ湴鍧�
+		'DB_NAME'               =>  'shop',          // 鏁版嵁搴撳悕
+		'DB_USER'               =>  'root',      // 鐢ㄦ埛鍚�
+		'DB_PWD'                =>  '',          // 瀵嗙爜
+				//'DB_USER'               =>  'zhoumeng',      // 鐢ㄦ埛鍚�
+		//'DB_PWD'                =>  '8520',          // 瀵嗙爜
+		/* 妯″潡鐩稿叧閰嶇疆 */
 		'DEFAULT_MODULE'     => 'Home',
 		'MODULE_DENY_LIST'   => array('Common'),
 		'MODULE_ALLOW_LIST'  => array('Home','Admin'),
