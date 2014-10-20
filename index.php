158c950f9b26c2d45b5da2a8ce3294a4a00a6409
Fingertipcc <Fingertipcc@163.com>
2014-10-20 09:21:10 +0800

    Revert "Update index.php"
    This reverts commit 51d9766bc62379ca92080518fc3c0118d8a7770b.

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ if (version_compare ( PHP_VERSION, '5.3.0', '<' ))
 	die ( 'require PHP > 5.3.0 !' );
 define ( 'APP_DEBUG', True );
 define ( 'APP_NAME', 'Orange' );
-define ( 'APP_PATH', './Apps/' );//123
+define ( 'APP_PATH', './Apps/' );
 require './ORG/ThinkPHP/ThinkPHP.php';
